b42c19ef577eb501b95648bc47ec1b4f3533f640
livinglabsicets <livinglabsicets@gmail.com>
2024-08-08 22:01:57 +0530

    first commit

---
+++ src/main/java/com/javabackend/banking/mapper/AccountMapper.java
@@ -0,0 +1,25 @@
+package com.javabackend.banking.mapper;
+
+import com.javabackend.banking.dto.AccountDto;
+import com.javabackend.banking.entity.Account;
+
+public class AccountMapper {
+
+    public static Account mapToAccount(AccountDto accountDto){
+        Account account = new Account(
+                accountDto.getId(),
+                accountDto.getAccountHolderName(),
+                accountDto.getBalance()
+        );
+        return account;
+    };
+    public static AccountDto mapToAccountDto(Account account){
+        AccountDto accountDto = new AccountDto(
+                account.getId(),
+                account.getAccountHolderName(),
+                account.getBalance()
+        );
+        return accountDto;
+
+    }
+}
---
+++ src/main/java/com/javabackend/banking/service/impl/AccountServiceImpl.java
@@ -0,0 +1,87 @@
+package com.javabackend.banking.service.impl;
+
+import com.javabackend.banking.dto.AccountDto;
+import com.javabackend.banking.entity.Account;
+import com.javabackend.banking.mapper.AccountMapper;
+import com.javabackend.banking.repository.AccountRepository;
+import com.javabackend.banking.service.AccountService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class AccountServiceImpl implements AccountService {
+
+    private AccountRepository accountRepository;
+
+    public AccountServiceImpl(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
+
+    @Override
+    public AccountDto createAccount(AccountDto accountDto) {
+        Account account= AccountMapper.mapToAccount(accountDto);
+        Account saveAccount=accountRepository.save(account);
+        return AccountMapper.mapToAccountDto(saveAccount);
+
+    }
+
+    @Override
+    public AccountDto getAccountById(Long id) {
+
+       Account account= accountRepository
+               .findById(id)
+               .orElseThrow(()-> new RuntimeException("Account does not exits"));
+
+        return AccountMapper.mapToAccountDto(account);
+    }
+
+    @Override
+    public AccountDto deposit(Long id, double amount) {
+        Account account=accountRepository
+                .findById(id)
+                .orElseThrow(()-> new RuntimeException("Account does not exits."));
+        double total= account.getBalance()+amount;
+        account.setBalance(total);
+        Account savedAccount=accountRepository.save(account);
+        return AccountMapper.mapToAccountDto(savedAccount);
+    }
+
+    @Override
+    public AccountDto withdraw(Long id, double amount) {
+
+        Account account=accountRepository
+                .findById(id)
+                .orElseThrow(()-> new RuntimeException("Account does not exits."));
+        if(account.getBalance()<amount)
+        {
+            throw new RuntimeException("Insufficient balance");
+        }
+        double total=account.getBalance()-amount;
+        account.setBalance(total);
+        Account savedAccount = accountRepository.save(account);
+
+        return AccountMapper.mapToAccountDto(savedAccount);
+    }
+
+    @Override
+    public List<AccountDto> getAllAccounts() {
+        List<Account> accounts= accountRepository.findAll();
+        return accounts.stream().map((account)->AccountMapper.mapToAccountDto(account))
+                .collect(Collectors.toList());
+
+    }
+
+    @Override
+    public void deleteAccount(Long id) {
+        Account account= accountRepository
+                .findById(id)
+                .orElseThrow(()-> new RuntimeException("Account does not exits"));
+
+        accountRepository.deleteById(id);
+
+    }
+
+
+}
